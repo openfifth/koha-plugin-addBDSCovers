1ca87056c91c6ef6f175189bfe5c76349bf2d1c0
Matt Blenkinsop <matt.blenkinsop@ptfs-europe.com>
2023-01-31 17:05:23 +0000

    v2.2

--- Koha/Plugin/AddBDSCovers.pm
+++ Koha/Plugin/AddBDSCovers.pm
@@ -4,13 +4,13 @@ use Modern::Perl;
 
 use base qw(Koha::Plugins::Base);
 
-our $VERSION = "2.1";
+our $VERSION = "2.2";
 
 our $metadata = {
     name            => 'AddBDSCovers',
     author          => 'Matt Blenkinsop',
     date_authored   => '2022-01-11',
-    date_updated    => "2022-01-30",
+    date_updated    => "2022-01-31",
     minimum_version => '19.05.00.000',
     maximum_version => undef,
     version         => $VERSION,
@@ -37,26 +37,14 @@ sub intranet_cover_images {
 	my $js = <<'JS';
     <script>
       function addBDSCovers(e) {
-        const cover_image_div = document.querySelector('.cover_images_required');
-        if(cover_image_div) {
-            let { isbn } = cover_image_div.dataset;
-            cover_image_div.innerHTML += `
-                <div class="cover-image">
-                    <a href="http://www.bibdsl.co.uk/xmla/image-service.asp?ISBN=${isbn}&amp;SIZE=l&amp;DBM=B" />
-                        <img src="http://www.bibdsl.co.uk/xmla/image-service.asp?ISBN=${isbn}&amp;SIZE=s&amp;DBM=B" alt="BDS cover image" />
-                    </a>
-                    <div class="hint">BDS cover image</div>
-                </div>
-            `;
-        }
-        const search_results_images = document.querySelectorAll('.search_cover_images_required');
+        const search_results_images = document.querySelectorAll('.cover_images_required');
         if(search_results_images.length > 0){
             search_results_images.forEach((div, i) => {
                 let { isbn, biblionumber, processedbiblio } = div.dataset
                 if(isbn){
                     div.innerHTML += `
-                        <div id="bds-coverimg-${biblionumber}" class="cover-image bds-coverimg">
-                            <a href=${processedbiblio}>
+                        <div id="bds-coverimg-${biblionumber}" class="cover-image">
+                            <a href=${ processedbiblio ? processedbiblio : `http://www.bibdsl.co.uk/xmla/image-service.asp?ISBN=${isbn}&amp;SIZE=l&amp;DBM=B` } >
                                 <img src="http://www.bibdsl.co.uk/xmla/image-service.asp?ISBN=${isbn}&amp;SIZE=s&amp;DBM=B" alt="BDS cover image" />
                             </a>
                             <div class="hint">BDS cover image</div>
@@ -81,27 +69,18 @@ sub opac_cover_images {
     my $js = <<'JS';
     <script>
       function addBDSCoversOPAC(e) {
-        const cover_image_div = document.querySelector('.cover_images_required');
-        if(cover_image_div) {
-            let { isbn } = cover_image_div.dataset.isbn;
-            cover_image_div.innerHTML += `
-                <div class="cover-image">
-                    <a href="http://www.bibdsl.co.uk/xmla/image-service.asp?ISBN=${cover_image_div.dataset.isbn}&amp;SIZE=l&amp;DBM=B" />
-                        <img src="http://www.bibdsl.co.uk/xmla/image-service.asp?ISBN=${cover_image_div.dataset.isbn}&amp;SIZE=s&amp;DBM=B" alt="BDS cover image" />
-                    </a>
-                </div>
-                <div class="hint">Image from BDS</div>
-            `;
-        }
-        const search_results_images = document.querySelectorAll('.search_cover_images_required');
+        const search_results_images = document.querySelectorAll('.cover_images_required');
         if(search_results_images.length > 0){
             search_results_images.forEach((div, i) => {
-                let { isbn, img_title } =  div.dataset;
+                let { isbn, imgtitle } =  div.dataset;
                 if(isbn){
                     div.innerHTML += `
-                        <span title="${img_title}" id="bds-thumbnail${i + 1}">
-                            <img src="http://www.bibdsl.co.uk/xmla/image-service.asp?ISBN=${isbn}&amp;SIZE=s&amp;DBM=B" alt="BDS cover image" class="item-thumbnail" />
-                        </span>
+                        <div class=${ imgtitle ? "" : "cover-image" }>
+                            <a href="http://www.bibdsl.co.uk/xmla/image-service.asp?ISBN=${isbn}&amp;SIZE=l&amp;DBM=B" />
+                                <img src="http://www.bibdsl.co.uk/xmla/image-service.asp?ISBN=${isbn}&amp;SIZE=s&amp;DBM=B" alt="BDS cover image" class=${ imgtitle ? "item-thumbnail" : "" } />
+                            </a>
+                        </div>
+                        <div class="hint">Image from BDS</div>
                     `;
                 } else {
                     div.innerHTML += `<span class="no-image">No cover image available</span>`;
